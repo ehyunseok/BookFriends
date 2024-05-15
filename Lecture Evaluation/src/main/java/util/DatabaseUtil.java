15556571e1d6a33f8b6ae87a58593685cadb05ed
ehyunseok <yhdaneys@gmail.com>
2024-05-15 22:06:10 +0900

    user data modeling

---
+++ Lecture Evaluation/src/main/java/util/DatabaseUtil.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseUtil {
+
+	public static Connection getConnection() {
+		try {
+			String dbURL = "jdbc:mysql://localhost:3306/lectureevaluation";
+			String dbID = "root";
+			String dbPassword = "mysql";
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("연결완료");
+			return DriverManager.getConnection(dbURL, dbID, dbPassword);
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
